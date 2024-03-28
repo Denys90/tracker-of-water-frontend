ed35dbcb6d42b098c3dc9019b69295c6f09c006f
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2024-03-28 13:51:15 +0100

    assembly preparation

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
