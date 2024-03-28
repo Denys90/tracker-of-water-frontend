d6f17930a107c513787163efcd51921bda2d6520
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-03-28 21:01:33 +0200

    rename page home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
-import FirstPage from 'pages/FirstPage/FirstPage';
+import HomePage from 'pages/HomePage/HomePage';
 import SecondPage from 'pages/SecondPage/SecondPage';
 import ErrorPage from 'pages/ErrorPage/ErrorPage';
 import { AppWrapper } from './App.styled';
@@ -10,12 +10,11 @@ import { globalStyles } from './styles/GlobalStyles.styled';
 function App() {
   return (
     <>
-      {' '}
       <Global styles={globalStyles} />
       <AppWrapper>
         <Routes>
           <Route path="/" element={<SharedLayout />}>
-            <Route path="/first" element={<FirstPage />} />
+            <Route path="/home" element={<HomePage />} />
             <Route path="/second" element={<SecondPage />}></Route>
             <Route path="*" element={<ErrorPage />} />
           </Route>
