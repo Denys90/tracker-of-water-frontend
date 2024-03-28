7e9daf5753fa51effe67b7d9cc68cd5ce220869b
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2024-03-28 14:05:46 +0100

    assembly preparation

--- src/pages/FirstPage/FirstPage.jsx
+++ src/pages/FirstPage/FirstPage.jsx
@@ -1,10 +1,8 @@
-import example from '../../assets/example.png';
-
 const FirstPage = () => {
   return (
     <div>
       <h2>First Page</h2>
-      <img src={example} alt="Example" />
+      <img src="#" alt="Example" />
     </div>
   );
 };
