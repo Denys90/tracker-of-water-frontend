b400d083ea0b9fcd90b6fb26be71edfbad416100
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-03-29 11:07:12 +0200

    styled

--- src/components/Norma/Norma.jsx
+++ src/components/Norma/Norma.jsx
@@ -1,9 +1,10 @@
-import styled from '@emotion/styled';
+import { NormaDivWrapper, NormaTitle } from './NormaStyled';
 
 export const Norma = () => {
   return (
     <NormaDivWrapper>
       <NormaTitle>My daily norma</NormaTitle>
+
       <div>
         <p>1.5 L</p>
         <button>Edit</button>
@@ -11,17 +12,3 @@ export const Norma = () => {
     </NormaDivWrapper>
   );
 };
-
-export const NormaDivWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
-
-export const NormaTitle = styled.h3`
-  font-size: 18px;
-  line-height: 24px;
-  font-weight: 500;
-  font-family: 'Roboto-Medium';
-  jc: flex;
-`;
---
+++ src/components/Norma/NormaStyled.js
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const NormaDivWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const NormaTitle = styled.h3`
+  font-size: 18px;
+  line-height: 24px;
+  font-weight: 500;
+  font-family: 'Roboto-Medium';
+`;
