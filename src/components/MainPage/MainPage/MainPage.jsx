2f23dedafe822403eb8159a4adb2fa7fc1c80908
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2024-03-30 13:16:12 +0100

    fixed err

--- src/components/MainPage/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage/MainPage.jsx
@@ -1,14 +1,15 @@
+import { ContainerStyles } from '../../Container/ContainerStyles.styled';
 import { ConsumptionTracker } from '../ConsumptionTracker/ConsumptionTracker';
 import { DrinkWater } from '../DrinkWater/DrinkWater';
 import { MainPageStyle } from './MainPage.styled';
 
 export const MainPage = () => {
   return (
-    <div>
+    <ContainerStyles>
       <MainPageStyle>
         <ConsumptionTracker />
         <DrinkWater />
       </MainPageStyle>
-    </div>
+    </ContainerStyles>
   );
 };
