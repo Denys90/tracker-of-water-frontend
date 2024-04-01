e01c6e3a83ee8ee46099820a69bc0be531211b7c
Denys90 <112502591+Denys90@users.noreply.github.com>
2024-04-01 19:05:24 +0000

    Deploying to gh-pages from @ Denys90/runners_of_code__frontend@79b51b2c7f75b57b08015cadad9e4bc97495c93c 🚀

---
+++ assets/ErrorPage-30200eb0.js
@@ -0,0 +1,38 @@
+import{n as e,t as o,d as r,j as t}from"./index-ffd7482d.js";const s=e.div`
+  position: relative;
+  background-color: ${o.colors.blue};
+  background-image: url(${r});
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 100vh;
+`,i=e.h1`
+  position: absolute;
+  top: 320px;
+  font-size: 111px;
+  margin: 0;
+  color: ${o.colors.blue};
+
+  text-shadow:
+    0 0 5px ${o.colors.white},
+    0 0 10px ${o.colors.white},
+    0 0 20px ${o.colors.white},
+    0 0 40px ${o.colors.white},
+    0 0 80px ${o.colors.white};
+`,n=e.h2`
+  font-size: 42px;
+  margin: 0;
+  color: ${o.colors.blue};
+  text-shadow:
+    0 0 5px ${o.colors.white},
+    0 0 10px ${o.colors.white},
+    0 0 20px ${o.colors.white},
+    0 0 40px ${o.colors.white},
+    0 0 80px ${o.colors.white};
+`,l=e.img`
+  position: absolute;
+  top: 249px;
+  filter: drop-shadow(5px 5px 10px #ffffff);
+`,c="/runners_of_code__frontend/assets/water-87d9a5e7.png",x=()=>t.jsxs(s,{children:[t.jsx(l,{src:c,alt:"water",width:"100"}),t.jsx(i,{children:"404"}),t.jsx(n,{children:"Page not found"})]});export{x as default};
---
+++ assets/LoginPage-2bcb9283.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-ffd7482d.js";const e=()=>t.jsx(t.Fragment,{});export{e as default};
