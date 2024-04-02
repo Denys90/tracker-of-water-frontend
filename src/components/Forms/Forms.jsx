39ff87e588a527f1d811e00fc5f4deda6b557f02
Denys Maksymchuk <112502591+Denys90@users.noreply.github.com>
2024-04-02 18:31:17 +0200

    form

--- src/components/Forms/Forms.jsx
+++ src/components/Forms/Forms.jsx
@@ -1,158 +1,160 @@
 import { useState } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { FormContainer, LabelField, LinkBtn, SubmitButton, Title, styles } from './forms.styled';
+import { Formik, Form } from 'formik';
+import {
+  FormContainer,
+  InputContainer,
+  InputField,
+  LabelField,
+  LinkBtn,
+  SubmitButton,
+  Title,
+  ErrorMessages,
+} from './AuthForm.styled';
+// import {
+//   FormContainer,
+//   LabelField,
+//   LinkBtn,
+//   SubmitButton,
+//   Title,
+//   styles,
+// } from './AuthForm.styled';
 
 const AuthForm = () => {
-    const [formType, setFormType] = useState('signin');
-    const [formSubmitted, setFormSubmitted] = useState(false);
-  
-    const handleFormTypeChange = type => {
-      setFormType(type);
-      setFormSubmitted(false);
-    };
-  
-    
-    return (
-      <FormContainer>
-        <Title>
-          {formType === 'signin' ? 'Sign In' : 'Sign Up'}
-        </Title>
-        <Formik
-          initialValues={{
-            email: '',
-            password: '',
-            repeatPassword: '',
-          }}
-          validate={values => {
-            const errors = {};
-            if (formSubmitted) {
-              if (formType === 'signin') {
-                if (!values.email) {
-                  errors.email = 'Email is required';
-                } else if (
-                  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                ) {
-                  errors.email = 'Some error message';
-                }
-                if (!values.password) {
-                  errors.password = 'Password is required';
-                } else if (values.password.length < 8) {
-                  errors.password = 'Password must be at least 8 characters long';
-                }
-              } else if (formType === 'signup') {
-                if (!values.email) {
-                  errors.email = 'Email is required';
-                } else if (
-                  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                ) {
-                  errors.email = 'Some error message';
-                }
-                if (!values.password) {
-                  errors.password = 'Password is required';
-                } else if (values.password.length < 8) {
-                  errors.password = 'Password must be at least 8 characters long';
-                }
-                if (!values.repeatPassword) {
-                  errors.repeatPassword = 'Please repeat your password';
-                } else if (values.password !== values.repeatPassword) {
-                  errors.repeatPassword = 'Passwords do not match';
-                }
+  const [formType, setFormType] = useState('signin');
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const handleFormTypeChange = (type) => {
+    setFormType(type);
+    setFormSubmitted(false);
+  };
+
+  return (
+    <FormContainer>
+      <Title>{formType === 'signin' ? 'Sign In' : 'Sign Up'}</Title>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+          repeatPassword: '',
+        }}
+        validate={(values) => {
+          const errors = {};
+          if (formSubmitted) {
+            if (formType === 'signin') {
+              if (!values.email) {
+                errors.email = 'Email is required';
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = 'Some error message';
+              }
+              if (!values.password) {
+                errors.password = 'Password is required';
+              } else if (values.password.length < 8) {
+                errors.password = 'Password must be at least 8 characters long';
+              }
+            } else if (formType === 'signup') {
+              if (!values.email) {
+                errors.email = 'Email is required';
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = 'Some error message';
+              }
+              if (!values.password) {
+                errors.password = 'Password is required';
+              } else if (values.password.length < 8) {
+                errors.password = 'Password must be at least 8 characters long';
+              }
+              if (!values.repeatPassword) {
+                errors.repeatPassword = 'Please repeat your password';
+              } else if (values.password !== values.repeatPassword) {
+                errors.repeatPassword = 'Passwords do not match';
               }
             }
-            return errors;
-          }}
-          onSubmit={(values, { setSubmitting, resetForm }) => {
-            console.log('Form submitted:', values);
-            setSubmitting(false);
-            resetForm();
-          }}
-        >
-          {({ isSubmitting, errors, resetForm }) => (
-            <Form>
-              <Field>
-                <LabelField htmlFor="email">
-                  Enter email
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          console.log('Form submitted:', values);
+          setSubmitting(false);
+          resetForm();
+        }}
+      >
+        {({ isSubmitting, resetForm }) => (
+          <Form>
+            <InputContainer>
+              <LabelField htmlFor="email">Enter email</LabelField>
+              <InputField
+                // css={[
+                //   styles.input,
+                //   formSubmitted && errors.email && styles.errorInput,
+                // ]}
+                type="email"
+                id="email"
+                name="email"
+                placeholder="E-mail"
+              />
+              <ErrorMessages name="email" component="div" />
+            </InputContainer>
+
+            <InputContainer>
+              <LabelField htmlFor="password">Enter password</LabelField>
+              <InputField
+                // css={[
+                //   styles.input,
+                //   formSubmitted && errors.password && styles.errorInput,
+                // ]}
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+              />
+              <ErrorMessages name="password" component="div" />
+            </InputContainer>
+
+            {formType === 'signup' && (
+              <InputContainer>
+                <LabelField htmlFor="repeatPassword">
+                  Repeat password
                 </LabelField>
-                <Field
-                  css={[
-                    styles.input,
-                    formSubmitted && errors.email && styles.errorInput,
-                  ]}
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="E-mail"
-                />
-                <ErrorMessage name="email" component="div" css={styles.error} />
-              </Field>
-  
-              <Field>
-                <LabelField htmlFor="password">
-                  Enter password
-                </LabelField>
-                <Field
-                  css={[
-                    styles.input,
-                    formSubmitted && errors.password && styles.errorInput,
-                  ]}
+                <InputField
+                  // css={[
+                  //   styles.input,
+                  //   formSubmitted && errors.repeatPassword && styles.errorInput,
+                  // ]}
                   type="password"
-                  id="password"
-                  name="password"
-                  placeholder="Password"
+                  id="repeatPassword"
+                  name="repeatPassword"
+                  placeholder="Repeat Password"
                 />
-                <ErrorMessage
-                  name="password"
-                  component="div"
-                  css={styles.error}
-                />
-              </Field>
-  
-              {formType === 'signup' && (
-                <Field>
-                  <LabelField htmlFor="repeatPassword">
-                    Repeat password
-                  </LabelField>
-                  <Field
-                    css={[
-                      styles.input,
-                      formSubmitted && errors.repeatPassword && styles.errorInput,
-                    ]}
-                    type="password"
-                    id="repeatPassword"
-                    name="repeatPassword"
-                    placeholder="Repeat Password"
-                  />
-                  <ErrorMessage
-                    name="repeatPassword"
-                    component="div"
-                    css={styles.error}
-                  />
-                </Field>
-              )}
-  
-              <SubmitButton
-                type="submit"
-                disabled={isSubmitting}
-                onClick={() => setFormSubmitted(true)}
-              >
-                {formType === 'signin' ? 'Sign In' : 'Sign Up'}
-              </SubmitButton>
-              <LinkBtn
-                type="button"
-                onClick={() => {
-                  resetForm();
-                  handleFormTypeChange(
-                    formType === 'signin' ? 'signup' : 'signin'
-                  );
-                }}
-              >
-                {formType === 'signin' ? 'Sign up' : 'Sign in'}
-              </LinkBtn>
-            </Form>
-          )}
-        </Formik>
-      </FormContainer>
-    );
-  };
-  
-  export default AuthForm;
+                <ErrorMessages name="repeatPassword" component="div" />
+              </InputContainer>
+            )}
+
+            <SubmitButton
+              type="submit"
+              disabled={isSubmitting}
+              onClick={() => setFormSubmitted(true)}
+            >
+              {formType === 'signin' ? 'Sign In' : 'Sign Up'}
+            </SubmitButton>
+            <LinkBtn
+              type="button"
+              onClick={() => {
+                resetForm();
+                handleFormTypeChange(
+                  formType === 'signin' ? 'signup' : 'signin'
+                );
+              }}
+            >
+              {formType === 'signin' ? 'Sign up' : 'Sign in'}
+            </LinkBtn>
+          </Form>
+        )}
+      </Formik>
+    </FormContainer>
+  );
+};
+
+export default AuthForm;
--- src/components/Forms/forms.styled.js
+++ src/components/Forms/forms.styled.js
@@ -1,109 +1,8 @@
-import styles from '@emotion/styled';
-// export const styles = {
-//     formContainer: css`
-//       display: flex;
-//       flex-direction: column;
-//       justify-content: flex-start;
-//       align-items: flex-start;
-//       padding: 0px;
-//       width: 384px;
-//     `,
-//     title: css`
-//       font-family: Roboto;
-//       font-size: 26px;
-//       font-weight: 500;
-//       line-height: 32px;
-//       width: 384px;
-//     `,
-//     formField: css`
-//       display: flex;
-//       flex-direction: column;
-//       justify-content: center;
-//       align-items: flex-start;
-//       padding: 0px;
-//       gap: 8px;
-//       margin-bottom: 16px;
-//       width: 384px;
-//     `,
-//     label: css`
-//       font-family: Roboto;
-//       font-size: 18px;
-//       font-weight: 400;
-//       line-height: 24px;
-//       width: 384px;
-//     `,
-//     input: css`
-//       display: flex;
-//       flex-direction: column;
-//       justify-content: center;
-//       align-items: flex-start;
-//       padding: 12px 10px 12px 10px;
-//       box-sizing: border-box;
-//       border: 1px solid rgb(215, 227, 255);
-//       border-radius: 6px;
-//       background: rgb(255, 255, 255);
-//       width: 384px;
-//       color: rgb(64, 123, 255);
-//       &::placeholder {
-//         color: rgb(158, 187, 255);
-//         font-family: Roboto;
-//         font-size: 16px;
-//         font-weight: 400;
-//         line-height: 20px;
-//       }
-//       &:focus {
-//         outline: none;
-//       }
-//     `,
-//     error: css`
-//       color: red;
-//       font-family: Roboto;
-//       font-size: 14px;
-//       font-weight: 400;
-//       line-height: 18px;
-//     `,
-//     submitButton: css`
-//       font-family: Roboto;
-//       font-size: 18px;
-//       font-weight: 500;
-//       line-height: 24px;
-//       letter-spacing: 0%;
-//       text-align: center;
-//       color: white;
-//       padding: 10px 30px 10px 30px;
-//       border-radius: 10px;
-//       box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
-//       background: rgb(64, 123, 255);
-//       width: 384px;
-//       border: none;
-//       cursor: pointer;
-//       transition: 0.4s;
-//       &:hover {
-//         box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.54);
-//       }
-//     `,
-//     link: css`
-//       color: rgb(64, 123, 255);
-//       font-family: Roboto;
-//       font-size: 16px;
-//       font-weight: 400;
-//       line-height: 20px;
-//       letter-spacing: 0%;
-//       text-decoration: none;
-//       transition: 0.4s;
-//       background-color: transparent;
-//       border: none;
-//       &:hover {
-//         color: rgb(255, 157, 67);
-//       }
-//     `,
-//     errorInput: css`
-//       border-color: rgb(239, 80, 80);
-//       color: rgb(239, 80, 80);
-//     `,
-//   };
+import styled from '@emotion/styled';
 
-export const FormContainer = styles.div`
+import { Field, ErrorMessage } from 'formik';
+
+export const FormContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
@@ -111,62 +10,101 @@ export const FormContainer = styles.div`
   padding: 0px;
   width: 384px;
 `;
-export const Title = styles.h2`
-      font-family: Roboto;
-      font-size: 26px;
-      font-weight: 500;
-      line-height: 32px;
-      width: 384px;
-    `;
-export const Field = styles.div`
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: flex-start;
-      padding: 0px;
-      gap: 8px;
-      margin-bottom: 16px;
-      width: 384px;
-    `;
-export const LabelField = styles.label`
-      font-family: Roboto;
-      font-size: 18px;
-      font-weight: 400;
-      line-height: 24px;
-      width: 384px;
+export const Title = styled.h2`
+  font-family: Roboto;
+  font-size: 26px;
+  font-weight: 500;
+  line-height: 32px;
+  width: 384px;
+`;
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 8px;
+  margin-bottom: 16px;
+  width: 384px;
+`;
+export const LabelField = styled.label`
+  font-family: Roboto;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 24px;
+  width: 384px;
+`;
+export const SubmitButton = styled.button`
+  font-family: Roboto;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 24px;
+  letter-spacing: 0%;
+  text-align: center;
+  color: white;
+  padding: 10px 30px 10px 30px;
+  border-radius: 10px;
+  box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
+  background: rgb(64, 123, 255);
+  width: 384px;
+  border: none;
+  cursor: pointer;
+  transition: 0.4s;
+  &:hover {
+    box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.54);
+  }
+`;
+export const LinkBtn = styled.button`
+  color: rgb(64, 123, 255);
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 20px;
+  letter-spacing: 0%;
+  text-decoration: none;
+  transition: 0.4s;
+  background-color: transparent;
+  border: none;
+  &:hover {
+    color: rgb(255, 157, 67);
+  }
+`;
+
+export const InputField = styled(Field)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 12px 10px 12px 10px;
+  box-sizing: border-box;
+  border: 1px solid rgb(215, 227, 255);
+  border-radius: 6px;
+  background: rgb(255, 255, 255);
+  width: 384px;
+  color: rgb(64, 123, 255);
+  &::placeholder {
+    color: rgb(158, 187, 255);
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 20px;
+  }
+  &:focus {
+    outline: none;
+  }
+
+  ${(props) =>
+    props.error &&
     `
-export const SubmitButton = styles.button`
-      font-family: Roboto;
-      font-size: 18px;
-      font-weight: 500;
-      line-height: 24px;
-      letter-spacing: 0%;
-      text-align: center;
-      color: white;
-      padding: 10px 30px 10px 30px;
-      border-radius: 10px;
-      box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
-      background: rgb(64, 123, 255);
-      width: 384px;
-      border: none;
-      cursor: pointer;
-      transition: 0.4s;
-      &:hover {
-        box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.54);
-      }
-`
-export const LinkBtn = styles.button`
-      color: rgb(64, 123, 255);
-      font-family: Roboto;
-      font-size: 16px;
-      font-weight: 400;
-      line-height: 20px;
-      letter-spacing: 0%;
-      text-decoration: none;
-      transition: 0.4s;
-      background-color: transparent;
-      border: none;
-      &:hover {
-        color: rgb(255, 157, 67);
-      }
-    `
+    border-color: rgb(239, 80, 80);
+    color: rgb(239, 80, 80);
+  `}
+`;
+
+export const ErrorMessages = styled(ErrorMessage)`
+  color: red;
+  font-family: Roboto;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 18px;
+`;
