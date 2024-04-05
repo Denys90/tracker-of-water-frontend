5b7cb0e3c853569c4210e92478b03ed99d5aa2f4
Denys90 <112502591+Denys90@users.noreply.github.com>
2024-04-05 16:56:27 +0000

    Deploying to gh-pages from @ Denys90/runners_of_code__frontend@4972e13a092a784a07da4815ee609d49da9cd671 🚀

--- assets/ErrorPage-76cdd92e.js
+++ assets/ErrorPage-76cdd92e.js
@@ -1,4 +1,4 @@
-import{n as e,t as o,b as r,j as t}from"./index-833f2062.js";const s=e.div`
+import{n as e,t as o,b as r,j as t}from"./index-5080b484.js";const s=e.div`
   position: relative;
   background-color: ${o.colors.blue};
   background-image: url(${r});
---
+++ assets/LoginPage-2848a612.js
@@ -0,0 +1,215 @@
+import{r as n,n as s,t as r,b as S,m as E,u as v,j as e}from"./index-5080b484.js";import{F as M,E as R,a as L,b as z}from"./formik.esm-155cd27b.js";const k=t=>n.createElement("svg",{width:15,height:13,viewBox:"0 0 15 13",fill:"none",xmlns:"http://www.w3.org/2000/svg",xmlnsXlink:"http://www.w3.org/1999/xlink",...t},n.createElement("desc",null,`
+			Created with Pixso.
+	`),n.createElement("defs",null),n.createElement("path",{id:"Vector",d:"M1.65 3.48C1.02 4.21 0.56 5.07 0.28 6C1.15 8.89 3.82 11 7 11C7.66 11 8.3 10.9 8.9 10.73M3.15 2.15C4.29 1.39 5.63 0.99 7 1C10.17 1 12.84 3.1 13.71 5.99C13.23 7.57 12.22 8.94 10.84 9.84M5.58 4.58L3.15 2.15L1 0M5.58 4.58C5.77 4.4 5.99 4.25 6.23 4.15C6.47 4.05 6.73 4 7 4C7.26 4 7.52 4.05 7.76 4.15C8 4.25 8.22 4.4 8.41 4.58C8.6 4.77 8.74 4.99 8.84 5.23C8.94 5.47 9 5.73 9 6C9 6.26 8.94 6.52 8.84 6.76C8.74 7 8.6 7.22 8.41 7.41L10.84 9.84L13 12M8.41 7.41L5.58 4.58",stroke:"#407BFF",strokeOpacity:1,strokeWidth:1,strokeLinejoin:"round"})),C=t=>n.createElement("svg",{width:16,height:16,viewBox:"0 0 16 16",fill:"none",xmlns:"http://www.w3.org/2000/svg",xmlnsXlink:"http://www.w3.org/1999/xlink",...t},n.createElement("desc",null,`
+			Created with Pixso.
+	`),n.createElement("defs",null,n.createElement("clipPath",{id:"clip146_844"},n.createElement("rect",{id:"eye/outline",width:16,height:16,fill:"white",fillOpacity:0}))),n.createElement("rect",{id:"eye/outline",width:16,height:16,fill:"#FFFFFF",fillOpacity:0}),n.createElement("g",{clipPath:"url(#clip146_844)"},n.createElement("path",{id:"Vector",d:"M1.35 7.78C2.28 5 4.9 3 8 3C11.09 3 13.71 5 14.64 7.78C14.68 7.92 14.68 8.07 14.64 8.21C13.71 10.99 11.09 13 8 13C4.9 13 2.28 10.99 1.35 8.21C1.31 8.07 1.31 7.92 1.35 7.78Z",stroke:"#407BFF",strokeOpacity:1,strokeWidth:1,strokeLinejoin:"round"}),n.createElement("path",{id:"Vector",d:"M9.41 9.41C9.03 9.78 8.53 10 8 10C7.46 10 6.96 9.78 6.58 9.41C6.21 9.03 6 8.53 6 8C6 7.46 6.21 6.96 6.58 6.58C6.96 6.21 7.46 6 8 6C8.53 6 9.03 6.21 9.41 6.58C9.78 6.96 10 7.46 10 8C10 8.53 9.78 9.03 9.41 9.41Z",stroke:"#407BFF",strokeOpacity:1,strokeWidth:1,strokeLinejoin:"round"}))),V="/runners_of_code__frontend/assets/background_element_home_mobile@1x-0bf10f90.png",Z="/runners_of_code__frontend/assets/background_element_home_mobile@2x-5c75a33c.png",A="/runners_of_code__frontend/assets/background_element_home_mobile@1x-0bf10f90.png",B="/runners_of_code__frontend/assets/background_element_home_mobile@2x-5c75a33c.png",O=s.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding: 0px;
+  width: 384px;
+  margin-right: 200px;
+  @media screen and (max-width: ${r.breakpoint.desktop}) {
+    margin-right: 0;
+    width: 336px;
+  }
+  @media screen and (max-width: ${r.breakpoint.tablet}) {
+    width: 280px;
+  }
+`,I=s.h2`
+  font-family: 'Roboto-Medium', sans-serif;
+  font-size: 26px;
+  font-weight: 500;
+  line-height: 32px;
+  width: 384px;
+  @media screen and (max-width: ${r.breakpoint.desktop}) {
+    width: 336px;
+  }
+  @media screen and (max-width: ${r.breakpoint.tablet}) {
+    width: 280px;
+  }
+`,l=s.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 8px;
+  margin-bottom: 16px;
+  width: 384px;
+  @media screen and (max-width: ${r.breakpoint.desktop}) {
+    width: 336px;
+  }
+  @media screen and (max-width: ${r.breakpoint.tablet}) {
+    width: 280px;
+  }
+`,c=s.label`
+  font-family: 'Roboto-Medium', sans-serif;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 24px;
+  width: 384px;
+  @media screen and (max-width: ${r.breakpoint.desktop}) {
+    width: 336px;
+  }
+  @media screen and (max-width: ${r.breakpoint.tablet}) {
+    width: 280px;
+  }
+`,T=s.button`
+  font-family: 'Roboto-Medium', sans-serif;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 24px;
+  letter-spacing: 0%;
+  text-align: center;
+  color: white;
+  padding: 10px 30px 10px 30px;
+  border-radius: 10px;
+  box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
+  background: rgb(64, 123, 255);
+  width: 384px;
+  border: none;
+  cursor: pointer;
+  transition: 0.4s;
+  &:hover {
+    box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.54);
+  }
+  @media screen and (max-width: ${r.breakpoint.desktop}) {
+    width: 336px;
+  }
+  @media screen and (max-width: ${r.breakpoint.tablet}) {
+    width: 280px;
+  }
+`,q=s.button`
+  margin-top: 16px;
+  color: rgb(64, 123, 255);
+  font-family: 'Roboto-Medium', sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 20px;
+  letter-spacing: 0%;
+  text-decoration: none;
+  transition: 0.4s;
+  background-color: transparent;
+  border: none;
+  &:hover {
+    color: rgb(255, 157, 67);
+  }
+`,m=s(M)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 12px 10px 12px 10px;
+  box-sizing: border-box;
+  border: 1px solid rgb(215, 227, 255);
+  border-radius: 6px;
+  background: rgb(255, 255, 255);
+  width: 384px;
+  color: rgb(64, 123, 255);
+  position: relative;
+  &::placeholder {
+    color: rgb(158, 187, 255);
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 20px;
+  }
+  &:focus {
+    outline: none;
+  }
+  @media screen and (max-width: ${r.breakpoint.desktop}) {
+    width: 336px;
+  }
+  @media screen and (max-width: ${r.breakpoint.tablet}) {
+    width: 280px;
+  }
+
+  ${t=>t.error&&`
+    border-color: rgb(239, 80, 80);
+    color: rgb(239, 80, 80);
+  `}
+
+  ${t=>t.error&&`
+    border-color: rgb(239, 80, 80);
+    color: rgb(239, 80, 80);
+  `}
+`,x=s(R)`
+  color: red;
+  font-family: 'Roboto-Medium', sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 18px;
+`,U=s.div`
+  background-size: cover;
+  background-position: center bottom;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-image: url(${V}), url(${Z});
+
+  @media screen and (min-width: ${r.breakpoint.tablet}) {
+    background-image: url(${A}), url(${B});
+    min-height: 100vh;
+  }
+  @media screen and (min-width: ${r.breakpoint.desktop}) {
+    background-image: url(${S}), url(${E});
+    min-height: 100vh;
+  }
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* width: 100%; */
+  margin: 0 auto;
+
+  @media screen and (min-width: ${r.breakpoint.tablet}) and (max-width: ${r.breakpoint.desktop}) {
+    flex-direction: row-reverse;
+    margin-left: 300px;
+  }
+    @media screen and (max-width:767px) {
+    flex-direction: column-reverse;
+    margin-top: 50px;
+  }
+`,W=s.img`
+  height: 588.5px;
+  @media screen and (min-width: ${r.breakpoint.tablet}) and (max-width: ${r.breakpoint.desktop}) {
+    height: 474.26px;
+    position: relative;
+    left: -200px;
+    top: 100px;
+  }
+  @media screen and (min-width: ${r.breakpoint.mobile}) and (max-width: 767px) {
+    height: 181.74px;
+  }
+`,X=s(k)`
+  position: absolute;
+  right: 10px;
+  width: 20px;
+  top: ${t=>t.error?"213px":"187px"};
+  height: auto;
+  cursor: pointer;
+`,D=s(k)`
+  position: absolute;
+  right: 10px;
+  width: 20px;
+  top: ${t=>t.error?"326px":"276px"};
+  height: auto;
+  cursor: pointer;
+`,G=s(C)`
+  position: absolute;
+  right: 10px;
+  width: 20px;
+  top: ${t=>t.error?"213px":"187px"};
+  height: auto;
+  cursor: pointer;
+`,H=s(C)`
+  position: absolute;
+  right: 10px;
+  width: 20px;
+  top: ${t=>t.error?"326px":"276px"};
+  height: auto;
+  cursor: pointer;
+`,J=()=>{const[t,_]=n.useState("signin"),[h,g]=n.useState(!1),[a,j]=n.useState(!1),[d,y]=n.useState(!1);console.log(h);const{signUp:$,signIn:F}=v(),P=o=>{_(o),g(!1)},w=()=>{j(!a)},b=()=>{y(!d)};return e.jsxs(U,{children:[e.jsx(W,{src:"/src/assets/images/mobile/bottle_sign_in@2x.png"}),e.jsxs(O,{children:[e.jsx(I,{children:t==="signin"?"Sign In":"Sign Up"}),e.jsx(L,{initialValues:{email:"",password:"",repeatPassword:""},validate:o=>{const i={};return h&&(t==="signin"?(o.email?/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(o.email)||(i.email="Some error message"):i.email="Email is required",o.password?o.password.length<8&&(i.password="Password must be at least 8 characters long"):i.password="Password is required"):t==="signup"&&(o.email?/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(o.email)||(i.email="Some error message"):i.email="Email is required",o.password?o.password.length<8&&(i.password="Password must be at least 8 characters long"):i.password="Password is required",o.repeatPassword?o.password!==o.repeatPassword&&(i.repeatPassword="Passwords do not match"):i.repeatPassword="Please repeat your password")),i},onSubmit:(o,{setSubmitting:i,resetForm:p})=>{const{email:u,password:f}=o;t==="signin"?F({email:u,password:f}):$({email:u,password:f}),console.log("Form submitted:",o),i(!1),i("values",o),p()},children:({isSubmitting:o,errors:i,resetForm:p})=>e.jsxs(z,{children:[e.jsxs(l,{children:[e.jsx(c,{htmlFor:"email",children:"Enter email"}),e.jsx(m,{error:i.email,type:"email",id:"email",name:"email",placeholder:"E-mail",autoComplete:"current-password"}),e.jsx(x,{name:"email",component:"div"})]}),e.jsxs(l,{children:[e.jsx(c,{htmlFor:"password",children:"Enter password"}),e.jsx(m,{error:i.password,type:a?"text":"password",id:"password",name:"password",placeholder:"Password",autoComplete:"current-password"}),a?e.jsx(e.Fragment,{children:e.jsx(G,{onClick:w,error:i.password})}):e.jsx(X,{onClick:w,error:i.password}),e.jsx(x,{name:"password",component:"div"})]}),t==="signup"&&e.jsxs(l,{children:[e.jsx(c,{htmlFor:"repeatPassword",children:"Repeat password"}),e.jsx(m,{error:i.repeatPassword,type:d?"text":"password",id:"repeatPassword",name:"repeatPassword",placeholder:"Repeat Password",autoComplete:"current-password"}),d?e.jsx(e.Fragment,{children:e.jsx(H,{onClick:b,error:i.password})}):e.jsx(D,{onClick:b,error:i.password}),e.jsx(x,{name:"repeatPassword",component:"div"})]}),e.jsx(T,{type:"submit",disabled:o,onClick:()=>g(!0),children:t==="signin"?"Sign In":"Sign Up"}),e.jsx(q,{type:"button",onClick:()=>{p(),P(t==="signin"?"signup":"signin")},children:t==="signin"?"Sign up":"Sign in"})]})})]})]})},Q=()=>e.jsx(e.Fragment,{children:e.jsx(J,{})});export{Q as default};
