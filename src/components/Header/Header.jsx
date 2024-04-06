b8d636a3f427658061a3c759b4153b4761ee9686
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2024-04-06 17:40:10 +0200

    added popover

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { UserLogo } from './UserLogo/UserLogo';
 
 import { Containers, ContainerHeader, WrapHeader } from './Header.styled';
 import useUsers from 'hooks/useUsers';
-import { UserAuth } from './UserAuth/UserAuth';
+import UserAuth from './UserAuth/UserAuth';
 
 const Header = () => {
   const { isAuthenticated } = useUsers();
