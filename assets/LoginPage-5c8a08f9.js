3e13eefff13a037a89155bdd0eb19fd9ac0a1388
Denys90 <112502591+Denys90@users.noreply.github.com>
2024-04-07 10:21:10 +0000

    Deploying to gh-pages from @ Denys90/runners_of_code__frontend@f8d41c4c74622b3d2c63ee39e628327993e0d24a 🚀

--- assets/LoginPage-5c8a08f9.js
+++ assets/LoginPage-5c8a08f9.js
@@ -1,4 +1,4 @@
-import{n as r,t as s,F as v,E as R,b as E,m as z,S as k,a as _,r as n,u as A,j as e,c as I,d as M}from"./index-25793e5a.js";const T="/runners_of_code__frontend/assets/background_element_home_mobile@1x-0bf10f90.png",Z="/runners_of_code__frontend/assets/background_element_home_mobile@2x-5c75a33c.png",V="/runners_of_code__frontend/assets/background_element_home_mobile@1x-0bf10f90.png",q="/runners_of_code__frontend/assets/background_element_home_mobile@2x-5c75a33c.png",U=r.div`
+import{n as r,t as s,F as C,E as R,b as E,m as z,S as k,a as _,r as n,u as A,j as e,c as I,d as M}from"./index-036a4ed6.js";const T="/runners_of_code__frontend/assets/background_element_home_mobile@1x-0bf10f90.png",Z="/runners_of_code__frontend/assets/background_element_home_mobile@2x-5c75a33c.png",V="/runners_of_code__frontend/assets/background_element_home_mobile@1x-0bf10f90.png",q="/runners_of_code__frontend/assets/background_element_home_mobile@2x-5c75a33c.png",U=r.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
@@ -94,7 +94,7 @@ import{n as r,t as s,F as v,E as R,b as E,m as z,S as k,a as _,r as n,u as A,j a
   &:hover {
     color: rgb(255, 157, 67);
   }
-`,c=r(v)`
+`,x=r(C)`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -124,16 +124,16 @@ import{n as r,t as s,F as v,E as R,b as E,m as z,S as k,a as _,r as n,u as A,j a
     width: 280px;
   }
 
-  ${o=>o.error&&`
+  ${i=>i.error&&`
     border-color: rgb(239, 80, 80);
     color: rgb(239, 80, 80);
   `}
 
-  ${o=>o.error&&`
+  ${i=>i.error&&`
     border-color: rgb(239, 80, 80);
     color: rgb(239, 80, 80);
   `}
-`,x=r(R)`
+`,c=r(R)`
   color: red;
   font-family: 'Roboto-Medium', sans-serif;
   font-size: 14px;
@@ -184,28 +184,28 @@ import{n as r,t as s,F as v,E as R,b as E,m as z,S as k,a as _,r as n,u as A,j a
   position: absolute;
   right: 10px;
   width: 20px;
-  top: ${o=>o.error?"213px":"187px"};
+  top: ${i=>i.error?"213px":"187px"};
   height: auto;
   cursor: pointer;
 `,J=r(k)`
   position: absolute;
   right: 10px;
   width: 20px;
-  top: ${o=>o.error?"326px":"276px"};
+  top: ${i=>i.error?"326px":"276px"};
   height: auto;
   cursor: pointer;
 `,K=r(_)`
   position: absolute;
   right: 10px;
   width: 20px;
-  top: ${o=>o.error?"213px":"187px"};
+  top: ${i=>i.error?"213px":"187px"};
   height: auto;
   cursor: pointer;
 `,N=r(_)`
   position: absolute;
   right: 10px;
   width: 20px;
-  top: ${o=>o.error?"326px":"276px"};
+  top: ${i=>i.error?"326px":"276px"};
   height: auto;
   cursor: pointer;
-`,Q=()=>{const[o,j]=n.useState("signin"),[h,g]=n.useState(!1),[a,$]=n.useState(!1),[d,y]=n.useState(!1);console.log(h);const{signUp:P,signIn:S,userInfo:F}=A(),C=i=>{j(i),g(!1)},w=()=>{$(!a)},b=()=>{y(!d)};return e.jsxs(D,{children:[e.jsx(G,{src:"/src/assets/images/mobile/bottle_sign_in@2x.png"}),e.jsxs(U,{children:[e.jsx(L,{children:o==="signin"?"Sign In":"Sign Up"}),e.jsx(I,{initialValues:{email:"",password:"",repeatPassword:""},validate:i=>{const t={};return h&&(o==="signin"?(i.email?/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(i.email)||(t.email="Some error message"):t.email="Email is required",i.password?i.password.length<8&&(t.password="Password must be at least 8 characters long"):t.password="Password is required"):o==="signup"&&(i.email?/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(i.email)||(t.email="Some error message"):t.email="Email is required",i.password?i.password.length<8&&(t.password="Password must be at least 8 characters long"):t.password="Password is required",i.repeatPassword?i.password!==i.repeatPassword&&(t.repeatPassword="Passwords do not match"):t.repeatPassword="Please repeat your password")),t},onSubmit:(i,{setSubmitting:t,resetForm:p})=>{const{email:u,password:f}=i;o==="signin"?S({email:u,password:f}):P({email:u,password:f}),console.log("Form submitted:",i),t(!1),t("values",i),F(),p()},children:({isSubmitting:i,errors:t,resetForm:p})=>e.jsxs(M,{children:[e.jsxs(l,{children:[e.jsx(m,{htmlFor:"email",children:"Enter email"}),e.jsx(c,{error:t.email,type:"email",id:"email",name:"email",placeholder:"E-mail",autoComplete:"current-password"}),e.jsx(x,{name:"email",component:"div"})]}),e.jsxs(l,{children:[e.jsx(m,{htmlFor:"password",children:"Enter password"}),e.jsx(c,{error:t.password,type:a?"text":"password",id:"password",name:"password",placeholder:"Password",autoComplete:"current-password"}),a?e.jsx(e.Fragment,{children:e.jsx(K,{onClick:w,error:t.password})}):e.jsx(H,{onClick:w,error:t.password}),e.jsx(x,{name:"password",component:"div"})]}),o==="signup"&&e.jsxs(l,{children:[e.jsx(m,{htmlFor:"repeatPassword",children:"Repeat password"}),e.jsx(c,{error:t.repeatPassword,type:d?"text":"password",id:"repeatPassword",name:"repeatPassword",placeholder:"Repeat Password",autoComplete:"current-password"}),d?e.jsx(e.Fragment,{children:e.jsx(N,{onClick:b,error:t.password})}):e.jsx(J,{onClick:b,error:t.password}),e.jsx(x,{name:"repeatPassword",component:"div"})]}),e.jsx(B,{type:"submit",disabled:i,onClick:()=>g(!0),children:o==="signin"?"Sign In":"Sign Up"}),e.jsx(O,{type:"button",onClick:()=>{p(),C(o==="signin"?"signup":"signin")},children:o==="signin"?"Sign up":"Sign in"})]})})]})]})},X=()=>e.jsx(e.Fragment,{children:e.jsx(Q,{})});export{X as default};
+`,Q=()=>{const[i,j]=n.useState("signin"),[h,g]=n.useState(!1),[a,$]=n.useState(!1),[d,y]=n.useState(!1);console.log(h);const{signUp:P,signIn:S,userInfo:F}=A(),v=o=>{j(o),g(!1)},b=()=>{$(!a)},w=()=>{y(!d)};return e.jsxs(D,{children:[e.jsx(G,{src:"/src/assets/images/mobile/bottle_sign_in@2x.png"}),e.jsxs(U,{children:[e.jsx(L,{children:i==="signin"?"Sign In":"Sign Up"}),e.jsx(I,{initialValues:{email:"",password:"",repeatPassword:""},validate:o=>{const t={};return h&&(i==="signin"?(o.email?/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(o.email)||(t.email="Some error message"):t.email="Email is required",o.password?o.password.length<8&&(t.password="Password must be at least 8 characters long"):t.password="Password is required"):i==="signup"&&(o.email?/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(o.email)||(t.email="Some error message"):t.email="Email is required",o.password?o.password.length<8&&(t.password="Password must be at least 8 characters long"):t.password="Password is required",o.repeatPassword?o.password!==o.repeatPassword&&(t.repeatPassword="Passwords do not match"):t.repeatPassword="Please repeat your password")),t},onSubmit:(o,{setSubmitting:t,resetForm:p})=>{const{email:f,password:u}=o;i==="signin"?S({email:f,password:u}):P({email:f,password:u}),console.log("Form submitted:",o),t(!1),t("values",o),setTimeout(()=>{F()},1e3),p()},children:({isSubmitting:o,errors:t,resetForm:p})=>e.jsxs(M,{children:[e.jsxs(l,{children:[e.jsx(m,{htmlFor:"email",children:"Enter email"}),e.jsx(x,{error:t.email,type:"email",id:"email",name:"email",placeholder:"E-mail"}),e.jsx(c,{name:"email",component:"div"})]}),e.jsxs(l,{children:[e.jsx(m,{htmlFor:"password",children:"Enter password"}),e.jsx(x,{error:t.password,type:a?"text":"password",id:"password",name:"password",placeholder:"Password"}),a?e.jsx(e.Fragment,{children:e.jsx(K,{onClick:b,error:t.password})}):e.jsx(H,{onClick:b,error:t.password}),e.jsx(c,{name:"password",component:"div"})]}),i==="signup"&&e.jsxs(l,{children:[e.jsx(m,{htmlFor:"repeatPassword",children:"Repeat password"}),e.jsx(x,{error:t.repeatPassword,type:d?"text":"password",id:"repeatPassword",name:"repeatPassword",placeholder:"Repeat Password"}),d?e.jsx(e.Fragment,{children:e.jsx(N,{onClick:w,error:t.password})}):e.jsx(J,{onClick:w,error:t.password}),e.jsx(c,{name:"repeatPassword",component:"div"})]}),e.jsx(B,{type:"submit",disabled:o,onClick:()=>g(!0),children:i==="signin"?"Sign In":"Sign Up"}),e.jsx(O,{type:"button",onClick:()=>{p(),v(i==="signin"?"signup":"signin")},children:i==="signin"?"Sign up":"Sign in"})]})})]})]})},X=()=>e.jsx(e.Fragment,{children:e.jsx(Q,{})});export{X as default};
