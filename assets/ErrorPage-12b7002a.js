1546b820e19f35bd33ab6079d8b54305280ae419
Denys90 <112502591+Denys90@users.noreply.github.com>
2024-04-09 10:57:45 +0000

    Deploying to gh-pages from @ Denys90/runners_of_code__frontend@6e650c64e8247190d222d3b26e5335081894df23 🚀

--- assets/ErrorPage-12b7002a.js
+++ assets/ErrorPage-12b7002a.js
@@ -1,4 +1,4 @@
-import{n as e,t as o,b as r,j as t}from"./index-87815d18.js";const s=e.div`
+import{n as e,t as o,b as r,j as t}from"./index-e1f22991.js";const s=e.div`
   position: relative;
   background-color: ${o.colors.blue};
   background-image: url(${r});
