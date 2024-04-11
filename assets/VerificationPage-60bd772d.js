17b578d583f7aced6fbbbbdad8a2499daf6abfe5
Denys90 <112502591+Denys90@users.noreply.github.com>
2024-04-11 06:57:02 +0000

    Deploying to gh-pages from @ Denys90/runners_of_code__frontend@1433a70a7f5f50f8a7a7918015051f8a1fb63688 🚀

---
+++ assets/VerificationPage-60bd772d.js
@@ -0,0 +1,1 @@
+import{j as t,N as a}from"./index-05e69e44.js";function o(){return t.jsx(a,{to:"/login"})}export{o as default};
--- assets/VerificationPage-857425bb.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as a}from"./index-37d39981.js";function o(){return t.jsx(a,{to:"/login"})}export{o as default};
