af649afad4f0a6d3634ea4e7c14f5d7e925eec44
Denys90 <112502591+Denys90@users.noreply.github.com>
2024-04-11 08:44:13 +0000

    Deploying to gh-pages from @ Denys90/runners_of_code__frontend@97cbbeaf5bad6d54dcf50284e6ae1130e31d4f25 🚀

--- assets/ErrorPage-ab720364.js
+++ assets/ErrorPage-ab720364.js
@@ -1,4 +1,4 @@
-import{n as e,t as o,m as r,j as t}from"./index-f7068405.js";const s=e.div`
+import{n as e,t as o,m as r,j as t}from"./index-0b493a85.js";const s=e.div`
   position: relative;
   background-color: ${o.colors.blue};
   background-image: url(${r});
--- assets/VerificationPage-4b482f05.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as a}from"./index-f7068405.js";function o(){return t.jsx(a,{to:"/login"})}export{o as default};
